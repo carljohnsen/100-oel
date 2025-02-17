e4c3bfd130974d54bb65641fa000166b3590e522
Carl Johnsen <carl-johannes@di.ku.dk>
2025-02-17 06:00:25 +0100

    Removed unintentional space

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 def get_spotify_client():
     # Set up the client credentials
-    scope = "app-remote-control,streaming,user-read-playback-state,user-modify-playback-state,user-read-currently-playing, playlist-modify-public"
+    scope = "app-remote-control,streaming,user-read-playback-state,user-modify-playback-state,user-read-currently-playing,playlist-modify-public"
     return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 def parse_environment():
