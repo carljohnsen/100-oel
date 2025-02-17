453e9996e1062f1115842d44350a35f4dae3fe7a
Carl Johnsen <carl-johannes@di.ku.dk>
2025-02-17 21:50:07 +0100

    Added the spotify playlist name as an argument

--- make_as_playlist.py
+++ make_as_playlist.py
@@ -2,8 +2,13 @@ import argparse
 import helpers
 import json
 
-parser = argparse.ArgumentParser(description='Creates a new playlist on spotify using the given tracklist. The playlist will be named "100 Øl"')
+parser = argparse.ArgumentParser(description='''Creates a new playlist on spotify
+                                 using the given tracklist. The playlist will
+                                 be named "100 Øl" or whatever is given as the
+                                 second argument.''')
 parser.add_argument('tracklist', type=str, help='The file containing the tracklist')
+parser.add_argument('playlist_name', type=str, help='''The name the playlist will
+                    have on Spotify.''', nargs='?', default='100 Øl')
 args = parser.parse_args()
 
 sp = helpers.get_spotify_client()
@@ -12,7 +17,7 @@ uid = sp.me()['id']
 with open(args.tracklist, 'r') as f:
     tracklist = json.load(f)["tracks"]
 
-sp.user_playlist_create(uid, '100 Øl', public=True)
+sp.user_playlist_create(uid, args.playlist_name, public=True)
 
 for track in tracklist:
     sp.user_playlist_add_tracks(uid, sp.current_user_playlists()['items'][0]['id'], [track['id']])
